3bdfced62ee26bba30937032ef6c12f5ae007a93
jahongir <abdurahmonov@gmail.com>
2021-01-20 14:02:33 +0100

    variable names

--- main.py
+++ main.py
@@ -22,3 +22,46 @@ if a > 10\
     print('a > 10 and b = 20')
 
 
+# identifier names
+# variable names are case-sensative
+
+my_VAR = 10
+my_var = 20
+
+# identifiers cannot be reserved words
+
+'''
+None
+True
+False
+and 
+or
+'''
+
+# when variable name starts with _ , this variable is private
+# private variables will not be imported
+# however, you still can access it
+
+_my_var = 20
+
+# __ is used for name mangleing
+
+__my_var = 30
+
+# some variable starts with __ and ends with __
+# you should not invent your __var__
+# there are many duner variables in Python
+
+__init__():
+
+# package names should be all lover case letters: utilities
+# for modules you can you _ : db_utils
+# classes uses upper camel case: BankAccount
+# function lover-cases, snake case: open_account
+# variable snake case: account_id
+# constants all-uppercase: MIN_APR
+
+# for more info on code style see PEP8
+
+
+
