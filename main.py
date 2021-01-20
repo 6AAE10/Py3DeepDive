ee0221ae3e27da9775f8d488427b15808b634038
jahongir <abdurahmonov@gmail.com>
2021-01-20 12:48:33 +0100

    first ever commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# So, this will be main fail where I will take notes while
+# doing Python 3 Deep Dive course.
+# Also, in order to get some experience with Git/GitHub
+# I will constantly commit and push the changes to GitHub repo.
+
+
+
+
