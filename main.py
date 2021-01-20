f183f8e01967c1fc56d8a3cc46df5f36de475228
jahongir <abdurahmonov@gmail.com>
2021-01-20 13:00:11 +0100

    line breaks are added

--- main.py
+++ main.py
@@ -3,6 +3,20 @@
 # Also, in order to get some experience with Git/GitHub
 # I will constantly commit and push the changes to GitHub repo.
 
+# Implicit and Explicit line breaks
+
+some_list = [10,
+             20,
+             30]
+
+def some_function(a,
+                  b,
+                  c):
+    return a, b, c
 
 
-
+a = 11
+b = 20
+if a > 10\
+    and b == 20:
+    print('a > 10 and b = 20')
